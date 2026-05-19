b5f1f74d285b231ce3bf90177fdc06867b948a68
Juninho Gava <juninhog@gmail.com>
2026-05-19 11:15:56 -0300

    Projetos iniciados api e ui

---
+++ dt-acc-api/src/main/java/br/com/desafio/accenture/AccentureApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.accenture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccentureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccentureApplication.class, args);
+	}
+
+}
