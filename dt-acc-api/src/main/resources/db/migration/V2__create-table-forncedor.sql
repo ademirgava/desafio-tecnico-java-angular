6af079c96e971eaea124bef9bff21921522393f3
Juninho Gava <juninhog@gmail.com>
2026-05-21 11:02:11 -0300

    Fix campo cnpj aceitar null

--- dt-acc-api/src/main/resources/db/migration/V2__create-table-forncedor.sql
+++ dt-acc-api/src/main/resources/db/migration/V2__create-table-forncedor.sql
@@ -1,8 +1,8 @@
 CREATE TABLE fornecedores( 
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     tipo_pessoa VARCHAR(8) NOT NULL,
-    cnpj VARCHAR(14) NOT NULL UNIQUE,
-    cpf VARCHAR(11) NOT NULL UNIQUE,
+    cnpj VARCHAR(14) UNIQUE,
+    cpf VARCHAR(11) UNIQUE,
     nome VARCHAR(120),
     cep VARCHAR(9),
     email VARCHAR(100),
