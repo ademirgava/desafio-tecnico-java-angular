15948dee9e5b48f638be7307cbc9921e701cda13
Juninho Gava <juninhog@gmail.com>
2026-05-22 14:35:29 -0300

    Iniciado implmentação da listagem e formulario de empresas

--- dt-acc-ui/src/app/paginas/empresa/lista-empresas/lista-empresas.component.ts
+++ dt-acc-ui/src/app/paginas/empresa/lista-empresas/lista-empresas.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { EmpresaService } from '../../../service/empresa/empresa.service';
+import { Router } from '@angular/router';
+import { Empresa } from '../../../components/model/empresa';
+import { MatTableDataSource } from '@angular/material/table';
+import { Column } from '../../../components/model/column';
+import { CommonModule } from '@angular/common';
+import { TableComponent } from '../../../components/table/table.component';
 
 @Component({
   selector: 'app-lista-empresas',
-  imports: [],
+  imports: [CommonModule, TableComponent],
   templateUrl: './lista-empresas.component.html',
   styleUrl: './lista-empresas.component.css',
 })
-export class ListaEmpresasComponent {}
+export class ListaEmpresasComponent implements OnInit {
+  private empresaService = inject(EmpresaService);
+  private router = inject(Router);
+
+  mensagemSemDados: string = 'Não há fornecedor cadastrado!';
+  tableData: Array<Empresa> = [];
+  dataSource: MatTableDataSource<Empresa> = new MatTableDataSource();
+
+  tableColumns: Array<Column> = [
+    {
+      columnDef: 'cnpj',
+      header: 'CNPJ',
+      cell: (element: Record<string, any>) => `${element['cnpj']}`,
+    },
+    {
+      columnDef: 'nomeFantasia',
+      header: 'Nome',
+      cell: (element: Record<string, any>) => `${element['nomeFantasia']}`,
+    },
+    {
+      columnDef: 'cep',
+      header: 'CEP',
+      cell: (element: Record<string, any>) => `${element['cep']}`,
+    },
+    {
+      columnDef: 'cidade',
+      header: 'Cidade',
+      cell: (element: Record<string, any>) => `${element['cidade']}`,
+    },
+  ];
+
+  ngOnInit(): void {}
+
+  novaEmpresa(): void {
+    this.router.navigateByUrl('/formulario-empresa');
+  }
+}
